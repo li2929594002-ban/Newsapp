f4815b6d14842081094d16d2f51b2389e385fe36
banban <li2929594002@gmail.com>
2026-08-30 15:20:09 +0800

    refactor: 清理死代码，移除无用mock注释与废弃代码

--- toutiao_backend/routers/users.py
+++ toutiao_backend/routers/users.py
@@ -22,20 +22,6 @@ async def register(user_data:UserRequest,db:AsyncSession = Depends(get_db)):   #
 
     token = await users.create_token(db,user.id)
 
-    # return {
-    #   "code": 200,
-    #   "message": "注册成功",
-    #   "data": {
-    #     "token": token,
-    #     "userInfo": {
-    #       "id": user.id,
-    #       "username": user.username,
-    #       "bio": user.bio,
-    #       "avatar": user.avatar
-    #     }
-    #   }
-    # }
-
     response_data = UserAuthResponse(token=token, userInfo= UserInfoResponse.model_validate(user))
     return success_response(message="注册成功", data = response_data)
 
